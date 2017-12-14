9f6b0743f23068d89fb07efa02dcae407975a4b7
Unknown <admin@site15.ru>
2017-12-14 19:54:04 +0500

    chore(deps): Update dependencies

--- srcgen/web-app-empty/apps/%app.name%/src/main.ts
+++ srcgen/web-app-empty/apps/%app.name%/src/main.ts
@@ -9,4 +9,4 @@ if (environment.production) {
 }
 
 platformBrowserDynamic().bootstrapModule(<%=app.classPrefix%>AppModule)
-  .catch(err => console.log(err));
+  .catch((err:any) => console.log(err));
