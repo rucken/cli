524a15b5d673f65262a27908e83e501b4f1e17fb
endy <admin@site15.ru>
2018-04-18 19:42:35 +0500

    fix(srcgen): Fix file names for "web-entity" script

--- test/cmd-generator-entity._spec.ts
+++ test/cmd-generator-entity._spec.ts
@@ -9,7 +9,7 @@ import { Base } from '../src/lib/base';
 const npmRun = require('npm-run');
 const assert = chai.assert;
 
-describe('Generator-grid-command: create base model, service and components for work with entity "apple"', () => {
+describe('Generator-entity-command: create base model, service and components for work with entity "apple"', () => {
     config();
     const debug = process.env.TEST_DEBUG === 'true';
     describe('#prepare()', () => {
@@ -21,8 +21,8 @@ describe('Generator-grid-command: create base model, service and components for 
             dir: _dir,
             dirLocal: `test/${_dirLocal}`,
             files: [
-                path.resolve(`${_dir}/src/shared/models/apple.model.ts`),
-                path.resolve(`${_dir}/src/shared/services/apples.service.ts`),
+                path.resolve(`${_dir}/src/shared/configs/apple.config.ts`),
+                path.resolve(`${_dir}/src/shared/models/apple.ts`),
             ]
         });
         _dirLocal = 'fixture/libs/subFolder/lib2'
@@ -31,22 +31,21 @@ describe('Generator-grid-command: create base model, service and components for 
             dir: _dir,
             dirLocal: `test/${_dirLocal}`,
             files: [
-                path.resolve(`${_dir}/src/grids/apples-grid/apple-modal/apple-modal.component.html`),
-                path.resolve(`${_dir}/src/grids/apples-grid/apple-modal/apple-modal.component.scss`),
-                path.resolve(`${_dir}/src/grids/apples-grid/apple-modal/apple-modal.component.ts`),
-                path.resolve(`${_dir}/src/grids/apples-grid/apple-modal/apple-modal.module.ts`),
-                path.resolve(`${_dir}/src/grids/apples-grid/apple-select-input/apple-select-input.component.html`),
-                path.resolve(`${_dir}/src/grids/apples-grid/apple-select-input/apple-select-input.component.scss`),
-                path.resolve(`${_dir}/src/grids/apples-grid/apple-select-input/apple-select-input.component.ts`),
-                path.resolve(`${_dir}/src/grids/apples-grid/apple-select-input/apple-select-input.module.ts`),
-                path.resolve(`${_dir}/src/grids/apples-grid/apples-grid.component.html`),
-                path.resolve(`${_dir}/src/grids/apples-grid/apples-grid.component.scss`),
-                path.resolve(`${_dir}/src/grids/apples-grid/apples-grid.component.ts`),
-                path.resolve(`${_dir}/src/grids/apples-grid/apples-grid.module.ts`),
-                path.resolve(`${_dir}/src/grids/apples-grid/apples-list-modal/apples-list-modal.component.html`),
-                path.resolve(`${_dir}/src/grids/apples-grid/apples-list-modal/apples-list-modal.component.scss`),
-                path.resolve(`${_dir}/src/grids/apples-grid/apples-list-modal/apples-list-modal.component.ts`),
-                path.resolve(`${_dir}/src/grids/apples-grid/apples-list-modal/apples-list-modal.module.ts`),
+                path.resolve(`${_dir}/src/components/entities/apple/apple-input/apple-input.component.html`),
+                path.resolve(`${_dir}/src/components/entities/apple/apple-input/apple-input.component.ts`),
+                path.resolve(`${_dir}/src/components/entities/apple/apple-input/apple-input.module.ts`),
+                path.resolve(`${_dir}/src/components/entities/apple/apple-modal/apple-modal.component.html`),
+                path.resolve(`${_dir}/src/components/entities/apple/apple-modal/apple-modal.component.ts`),
+                path.resolve(`${_dir}/src/components/entities/apple/apple-modal/apple-modal.module.ts`),
+                path.resolve(`${_dir}/src/components/entities/apple/apple-select/apple-select.component.html`),
+                path.resolve(`${_dir}/src/components/entities/apple/apple-select/apple-select.component.ts`),
+                path.resolve(`${_dir}/src/components/entities/apple/apple-select/apple-select.module.ts`),
+                path.resolve(`${_dir}/src/components/entities/apple/apples-grid/apples-grid.component.html`),
+                path.resolve(`${_dir}/src/components/entities/apple/apples-grid/apples-grid.component.ts`),
+                path.resolve(`${_dir}/src/components/entities/apple/apples-grid/apples-grid.module.ts`),
+                path.resolve(`${_dir}/src/components/entities/apple/apples-grid-modal/apples-grid-modal.component.html`),
+                path.resolve(`${_dir}/src/components/entities/apple/apples-grid-modal/apples-grid-modal.component.ts`),
+                path.resolve(`${_dir}/src/components/entities/apple/apples-grid-modal/apples-grid-modal.module.ts`)
             ]
         });
         beforeEach(() => {
@@ -99,9 +98,9 @@ describe('Generator-grid-command: create base model, service and components for 
                 done(e);
             });
         });
-        it(`rucken grid --entity-name apple --root ./test/fixture`, (done) => {
+        it(`rucken entity --entity-name apple --root ./test/fixture`, (done) => {
             const file = path.resolve(`${dirRoot}/dist/bin/app.js`);
-            const commandString = 'node . grid --entity-name apple --root ./test/fixture ' + (debug ? ' --verbose' : '');
+            const commandString = 'node . entity --entity-name apple --root ./test/fixture ' + (debug ? ' --verbose' : '');
 
             assert.equal(fsExtra.existsSync(file), true);
 
@@ -113,13 +112,12 @@ describe('Generator-grid-command: create base model, service and components for 
                     dirLocal: dirLocal,
                     files: files
                 }) => {
-                    assert.equal(true, true);/*
                     files.forEach((file: string) => {
                         if (!fsExtra.existsSync(file)) {
                             console.log(file);
                         }
                         assert.equal(fsExtra.existsSync(file), true);
-                    });*/
+                    });
                 });
                 done();
             }).catch((e: any) => {
