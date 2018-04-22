6a58cdd0b33ca7a1a3b4051f53f2c5c185b46ef9
endy <admin@site15.ru>
2018-04-22 23:10:16 +0500

    fix(srcgen): Remove duplicated imports from modules for "web-entity" template

--- srcgen/web-entity/%libs.platformFolder%/src/components/entities/%entity.name.kebab%/%entity.list.name.kebab%-grid/%entity.list.name.kebab%-grid.module.ts
+++ srcgen/web-entity/%libs.platformFolder%/src/components/entities/%entity.name.kebab%/%entity.list.name.kebab%-grid/%entity.list.name.kebab%-grid.module.ts
@@ -1,29 +1,22 @@
 import { CommonModule } from '@angular/common';
 import { ModuleWithProviders, NgModule } from '@angular/core';
-import { TranslateModule } from '@ngx-translate/core';
-import { ModalModule } from 'ngx-bootstrap/modal';
-import { PipesModule } from '@rucken/web';
 import { EntityGridModule } from '@rucken/web';
-import { EntityModalModule, MessageModalModule } from '@rucken/web';
 import { <%=entity.name.camel%>ModalModule } from '../<%=entity.name.kebab%>-modal/<%=entity.name.kebab%>-modal.module';
 import { <%=entity.list.name.camel%>GridComponent } from './<%=entity.list.name.kebab%>-grid.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    MessageModalModule,
-    TranslateModule.forChild(),
     EntityGridModule,
-    EntityModalModule,
-    <%=entity.name.camel%>ModalModule,
-    ModalModule.forRoot(),
-    PipesModule
+    <%=entity.name.camel%>ModalModule
   ],
   declarations: [
     <%=entity.list.name.camel%>GridComponent
   ],
   exports: [
-    <%=entity.list.name.camel%>GridComponent
+    <%=entity.list.name.camel%>GridComponent,
+    EntityGridModule,
+    <%=entity.name.camel%>ModalModule
   ]
 })
 export class <%=entity.list.name.camel%>GridModule {
