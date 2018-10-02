975cc8d4209fb51aaead0432665cc2c9ed0cbb4d
EndyKaufman <admin@site15.ru>
2018-10-03 00:21:16 +0500

    chore: Update push-generators-outputs.sh

--- .travis/push-generators-outputs.sh
+++ .travis/push-generators-outputs.sh
@@ -3,30 +3,43 @@
 setup_git() {
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis CI"
+  git init
+  git remote add origin https://${GH_TOKEN}@github.com/rucken/cli.git > /dev/null 2>&1
+  git fetch origin generators-outputs
+  git pull origin generators-outputs
 }
 
-commit_website_files() {
-  git init
+commit_files() {
   git checkout -b generators-outputs
   git add .
   git commit --message "$TRAVIS_COMMIT"
 }
 
 upload_files() {
-  git remote add origin https://${GH_TOKEN}@github.com/rucken/cli.git > /dev/null 2>&1
-  git fetch origin generators-outputs
-  git pull origin generators-outputs
   git push --quiet --set-upstream origin generators-outputs 
 }
 
-run_generators(){
+clear_fixtures(){
   rimraf ./test/fixtures
   mkdir ./test/fixtures
+}
+
+run_generators(){
   npm run run-generators
+}
+
+move_down(){
   cd ./test/fixtures
 }
 
-run_generators
+move_up(){
+  cd ..
+}
+
+clear_fixtures
+move_down
 setup_git
-commit_website_files
+run_generators
+move_down
+commit_files
 upload_files
